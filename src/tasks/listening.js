ec28db4a5033a67c50622ad408569e7718e7ec40
Yura-Korbut <korbuty@mail.ru>
2018-06-17 12:56:07 +0300

    fix/changed voice of web speech API

--- src/tasks/listening.js
+++ src/tasks/listening.js
@@ -15,12 +15,17 @@ export default function listening() {
     repeat.classList.remove('hidden');
 
     taskExplanation.innerText ='Введите произнесенное слово.';
+    let voices = speechSynthesis.getVoices();
     let result =_.sample(Object.keys(vocabulary));
     utterThis = new SpeechSynthesisUtterance();
     utterThis.text = result;
     utterThis.rate = 0.5;
     utterThis.lang = 'en-US';
-    utterThis.voice = 'Google US English';
+    for (let i = 0; i < voices.length ; i++) {
+        if(voices[i].name === 'Google US English') {
+          utterThis.voice = voices[i];
+        }
+      }
     utterThis.volume = 100;
     synth.speak(utterThis);
 
